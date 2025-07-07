c0dba42bfdb7427aa842fa1ff0ffc2c92b0fe785
jjjjjjjiwon <hgw7501@gmail.com>
2025-07-07 14:01:39 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,22 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, anyone");
+        ;
+        Console.WriteLine(Solution070(100, 2));
+    }
+
+    public static int Solution0704(int num1, int num2)
+    {
+        int answer = 0;
+        answer = num1 % num2;
+        return answer;
+    }
+
+    public static int Solution070(int num1, int num2)
+    {
+        int answer = 0;
+        answer = num1 - num2;
+        return answer;
     }
 }
+
