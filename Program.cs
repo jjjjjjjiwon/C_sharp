c13654d5b5f26c77b55e05faa61adc1d50f38a79
jjjjjjjiwon <hgw7501@gmail.com>
2025-07-07 14:25:50 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,22 @@
     private static void Main(string[] args)
     {
         ;
-        Console.WriteLine(Solution070(100, 2));
+        Console.WriteLine(Solution0707_2(27, 19));
+    }
+
+
+    public static int Solution0707_2(int num1, int num2)
+    {
+        int answer = 0;
+        answer = num1 * num2;
+        return answer;
+    }
+
+    public static int Solution0707(int num1, int num2)
+    {
+        int answer = 0;
+        answer = num1 - num2;
+        return answer;
     }
 
     public static int Solution0704(int num1, int num2)
@@ -13,11 +28,6 @@
         return answer;
     }
 
-    public static int Solution070(int num1, int num2)
-    {
-        int answer = 0;
-        answer = num1 - num2;
-        return answer;
-    }
+
 }
 
