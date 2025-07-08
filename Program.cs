c074aba608040fa01705269901309e9cfcca1ab1
jjjjjjjiwon <hgw7501@gmail.com>
2025-07-08 11:34:45 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,9 +7,21 @@
     /// <param name="args"></param>
     private static void Main(string[] args)
     {
-        Console.WriteLine(Solution0707_2(27, 19));
+        Console.WriteLine(Solution0708(7, 2));
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="num1"></param>
+    /// <param name="num2"></param>
+    /// <returns></returns>
+    public static int Solution0708(int num1, int num2)
+    {
+        int answer = 0;
+        answer = num1 / num2;
+        return answer;
+    }
 
     /// <summary>
     /// 두 수의 곱하기 연산
